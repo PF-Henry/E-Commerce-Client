76de2c7d63a0cad51b2abd243800b4240d342bba
victips <torvicarias@gmail.com>
2022-07-06 20:26:40 -0500

    Se renderizan los componentes en el Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Cards from "../Cards/Cards";
+import Filteredbar from "../Filteredbar/Filteredbar";
+import Offers from "../Offers/Offers";
+import Header from "../Header/Header";
+import Brands from "../Brands/Brands";
+import Footer from "../Footer/Footer";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      Home
+      <Header />
+      <Filteredbar />
+      <Cards />
+      <Offers />
+      <Brands />
+      <Footer />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
---
+++ src/Components/Offers/Offers.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card } from "../Card/Card";
+
+const Offers = () => {
+  return (
+    <div>
+      Offers
+      <Card />
+    </div>
+  );
+};
+
+export default Offers;
