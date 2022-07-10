ca8093221a88853a26133c1b1c88823549426007
victips <torvicarias@gmail.com>
2022-07-10 15:46:57 -0500

    Se agrega formulario de creación de productos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
+import CreateForm from "./Components/CreateForm/CreateForm";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/CreateProduct" element={<CreateForm />} />
       </Routes>
     </div>
   );
