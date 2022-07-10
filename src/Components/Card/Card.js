d87f77d01da7ba0803e4f60624bfc0bedc34bab9
victips <torvicarias@gmail.com>
2022-07-09 21:39:38 -0500

    Se modifica el estilo de las cartas para que el precio y el btn de agregar al carrito queden siempre abajo

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -13,17 +13,21 @@ const Card = ({ object }) => {
         className="card-img-top productImage"
         alt="item"
       />
-      <div className="card-body">
-        <h5 className="card-title text-blue mb-3 letter-spacing">
-          {object.name}
-        </h5>
-        <p className="card-text letter-spacing fw-light">
-          {object.description.slice(0, 100)}
-          {object.description.length > 100 && "..."}
-        </p>
-        <h1 className="text-blue mb-3 letter-spacing">${object.price}</h1>
-        <div className="btn text-white bg-purple-dark py-1 addToCartBtn border-0 letter-spacing">
-          Add to the cart
+      <div className="card-body d-flex flex-column">
+        <div className="mb-3">
+          <h5 className="card-title text-blue mb-3 letter-spacing">
+            {object.name}
+          </h5>
+          <p className="card-text letter-spacing fw-light">
+            {object.description.slice(0, 100)}
+            {object.description.length > 100 && "..."}
+          </p>
+        </div>
+        <div className="mt-auto">
+          <h1 className="text-blue mb-3 letter-spacing">${object.price}</h1>
+          <div className="btn text-white bg-purple-dark py-1 addToCartBtn border-0 letter-spacing">
+            Add to the cart
+          </div>
         </div>
       </div>
     </div>
