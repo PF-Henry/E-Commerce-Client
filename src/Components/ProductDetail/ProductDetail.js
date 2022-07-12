c11b017ebd2381e04bb94c3c03a73680da95a04c
Ricardo Vargas <rich.vartro@gmail.com>
2022-07-11 21:21:15 -0600

    Detail Page Ready

--- src/Components/ProductDetail/ProductDetail.js
+++ src/Components/ProductDetail/ProductDetail.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import { getDetailProductAsync } from '../../Redux/productSlice';
 import { Footer } from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
@@ -55,10 +55,13 @@ export const ProductDetail = () => {
     <div>
         <Navbar />
         <div className='div-container'>
-            <h1>{productDetails.name}</h1>
+            <div className='div-container-header'>
+                <NavLink to='/' className='div-container-header--btn animate__animated animate__fadeInLeft' >Back</NavLink>
+                <h1 className="titleDetail animate__animated animate__fadeInRight" >{productDetails.name}</h1>
+            </div>
             
             <div className='div-container-sectionOne'>
-                <div className='div-container-sectionOne-One'>
+                <div className='div-container-sectionOne-One animate__animated animate__fadeInLeft'>
                     <div className='div-container-sectionOne-One-Picture--One'>
                         <img src={productDetails.img1} alt={productDetails.name} className='img-fluid'/>
                     </div>
@@ -73,20 +76,60 @@ export const ProductDetail = () => {
         
                 </div>
 
-                <div className='div-container-sectionOne-Two'>
+                <div className='div-container-sectionOne-Two animate__animated animate__fadeIn'>
                     <img src={productDetails.img1} alt={productDetails.name} className='img-fluid'/>
                 </div>
 
-                <div className='div-container-sectionOne-Three'>
+                <div className='div-container-sectionOne-Three animate__animated animate__fadeInRight'>
+                    <div className='div-info-product' > 
+                        <h2 className='div-info-product-title' >Price:</h2>
+                        <h3 className='div-info-product-description--price' > $  {productDetails.price}</h3>
+                    </div>
 
+                    <div className='div-info-product' > 
+                        <div className='div-info-section'>
+                            <h2 className='div-info-product-title' >Branch:</h2>
+                            <h3 className='div-info-product-description' > {productDetails.brandName}</h3>
+                        </div>
+
+                        <div className='div-info-section'>
+                            <h2 className='div-info-product-title' >Stock:</h2>
+                            <h3 className='div-info-product-description' > {productDetails.stock}</h3>
+                        </div>
+
+                    </div>
+
+                    <div className='div-info-product' > 
+                        <h2 className='div-info-product-title' >Categories:</h2>
+                        <h3 className='div-info-product-description' > {productDetails.categories}</h3>
+                    </div>
+
+                    <div className='div-info-btn' >
+                        <select className='select-quantity'>
+                            <option disable>Select Quantity</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                        </select>
+                        <button className='btnDetail-Add-Product' > Add to the cart</button>
+                    </div>
                 </div>
             </div>
 
-            <div className='div-container-sectionTwo'>
-            <h3> $  {productDetails.price}</h3>
-            <h3> {productDetails.stock}</h3>
-            <h3> {productDetails.technical_especification}</h3>
-            <h3> {productDetails.categories}</h3>
+            <div className='div-container-sectionTwo animate__animated animate__fadeInUp'>
+                <div className='div-info-technical-decription'>
+                    <h3 className='div-info-technical-decription--title'>Technical Description</h3>
+                    <p className='div-info-technical-decription--p'> {productDetails.technical_especification}</p>
+                </div>
+
+                <div className='divSepareted'></div>
+
+                <div className='div-info-technical-decription'>
+                    <h3 className='div-info-technical-decription--title'>Reviews</h3>
+                </div>
             </div>
         </div>
 
