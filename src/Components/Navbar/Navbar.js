b952efe35c624c7b166e8f3f0571730884c3e127
victips <torvicarias@gmail.com>
2022-07-13 13:02:11 -0500

    Se modifican los estilos de la searchBar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -36,29 +36,6 @@ const Navbar = () => {
             className="collapse navbar-collapse display_around"
             id="navbarSupportedContent"
           >
-            {/* <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Dropdown
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href="#">Action</a>
-                  <a className="dropdown-item" href="#">Another action</a>
-                  <div className="dropdown-divider"></div>
-                  <a className="dropdown-item" href="#">Something else here</a>
-                </div>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="#">Disabled</a>
-              </li>
-            </ul> */}
-
             <Searchbar />
 
             <ul className="navbar-nav">
@@ -69,7 +46,9 @@ const Navbar = () => {
                 to="/admin"
                 className="adminNavLink d-flex align-items-center gap-1 aqua-hover"
               >
-                <li className="nav-item letter-spacing nav-li-font ">SIGN IN</li>
+                <li className="nav-item letter-spacing nav-li-font ">
+                  SIGN IN
+                </li>
                 <CgProfile size={"1.6rem"} />
               </Link>
             </ul>
--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { searchProductAsync } from "../../Redux/productSlice";
+import { FiSearch } from "react-icons/fi";
 import "./Searchbar.css";
 
 const Searchbar = () => {
@@ -18,15 +19,18 @@ const Searchbar = () => {
   };
 
   return (
-    <div className="div--Serchbar">
+    <div className="input-group input-group-sm div--Serchbar">
       <input
         value={search}
-        className="input--SearchBar"
+        className="form-control input--SearchBar"
         placeholder="Search Product"
         onChange={(e) => handleInputChange(e)}
       />
-      <button className="btn--Searchbar" onClick={(e) => handleSubmit(e)}>
-        SEARCH
+      <button
+        className="btn--Searchbar d-flex align-items-center px-3"
+        onClick={(e) => handleSubmit(e)}
+      >
+        <FiSearch size={"1.1rem"} />
       </button>
     </div>
   );
