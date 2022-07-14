2f32500ced79416d9337262f293ec08fe3d4f7b1
victips <torvicarias@gmail.com>
2022-07-14 11:20:13 -0500

    Se agrega un estilo básico a la página del carrito

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import { Footer } from "../../Components/Footer/Footer";
+import { FiAlertTriangle } from "react-icons/fi";
+import "./Cart.css";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="container-md  min-vh-65">
+        <div className="fs-1 text-start w-100 formH1 mb-3">Shopping Cart</div>
+        <div className="text-start alert alert-warning fs-5 d-flex align-items-center gap-2">
+          <FiAlertTriangle size={"1.8rem"} />
+          Your Shopping Cart is Empty.
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Cart;
