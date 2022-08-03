57d787832ddf6d2e7ebd21cf64b45bc7ff2d49f5
Ricardo Vargas <rich.vartro@gmail.com>
2022-08-03 14:24:22 -0600

    fixed tabIndex

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -98,7 +98,7 @@ export const Login = () => {
     <div className="padding-container--login padding-container">
 
     {/* <!-- Modal --> */}
-    <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div className="modal fade" id="exampleModalCenter" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div className="modal-dialog modal-dialog-centered" role="document">
         <div className="modal-content">
           <div className="modal-header">
